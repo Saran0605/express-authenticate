5527479d2800f0c159e007e3f25c4ed59dba9e39
Saran S <141417701+Saran0605@users.noreply.github.com>
2025-04-28 16:03:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 app.use('/todo',todo);
+app.use('/hello',async(req,res)=>{
+    res.send("Hi Prettt");
+})
 app.listen(port,()=>{
     console.log("serve started");
 });
