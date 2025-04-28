0389160cf4a822d004dc46eeafe59df69cf635dc
Saran S <141417701+Saran0605@users.noreply.github.com>
2025-04-28 16:40:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const todo = require('./route');
 const app = express();
 const port = 3000;
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Hello from Express inside Docker!');
+});
 app.use('/todo',todo);
 app.use('/hello',async(req,res)=>{
     res.send("Hi Prettt");
